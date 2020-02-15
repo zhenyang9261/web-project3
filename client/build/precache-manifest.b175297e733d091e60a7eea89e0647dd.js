6f454897c0dfdfd7999179df89fbd18a363724a4
Broba_Fett <brandon.j.rosser@gmail.com>
2020-02-15 10:29:42 -0500

    updating from last pull

---
+++ client/build/precache-manifest.b175297e733d091e60a7eea89e0647dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a6b8ca88b1af5922c0d8",
+    "url": "/static/js/main.f9f81226.chunk.js"
+  },
+  {
+    "revision": "11ba2713b20691d36789",
+    "url": "/static/js/2.246ad172.chunk.js"
+  },
+  {
+    "revision": "a6b8ca88b1af5922c0d8",
+    "url": "/static/css/main.e9e4cfd0.chunk.css"
+  },
+  {
+    "revision": "995dc499b02b15d087e84932bd6a7000",
+    "url": "/index.html"
+  }
+];
